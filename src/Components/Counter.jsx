a7fdb373f18a8f20fed350ed9361367a26c71eb4
Shane-McDonald33 <mcdonaldshane33@gmail.com>
2022-02-28 22:51:33 -0600

    added more states and dispatches, more functionality

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -6,23 +6,31 @@ import classes from './Counter.module.css';
 const Counter = () => { //useSelector allows you to select specific pieces from your store
   const dispatch = useDispatch();//directly calls back to the store's action labels
   const counter = useSelector(state => state.counter); //state.counter being the defualt state that can be affected by dispatched actions
+  const show = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: 'INCREMENT'})//must match exactly as labeled in the store
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: 'INCREASE', amount: 5})//property "amount" here needs to match with the propery labeled back in the store, as in, "action.amount"
   };
 
   const decrementHandler = () => {
     dispatch({ type: 'DECREMENT'})//must match exactly as it does in the store
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'TOGGLE'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/Store/index.jsx
+++ src/Store/index.jsx
@@ -2,15 +2,31 @@
 
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0}, action) => {//default state
+const initialState = { counter: 0, showCounter: true}
+
+const counterReducer = (state = initialState, action) => {//default state
     if (action.type === 'INCREMENT') {//action statement
         return {
-            counter: state.counter + 1//what the action does to the state
+            counter: state.counter + 1,//what the action does to the state
+            showCounter: state.showCounter
+        }
+    }
+    if (action.type === 'INCREASE') {
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
         }
     }
     if (action.type === 'DECREMENT') {//action statement
+        return {   
+            counter: state.counter - 1, //what is does to the state
+            showCounter: state.showCounter
+        }
+    }
+    if (action.type === 'TOGGLE') {
         return {
-            counter: state.counter - 1 //what is does to the state
+            showCounter: !state.showCounter,
+            counter: state.counter
         }
     }
     return state//return default state if neither if statements are met
